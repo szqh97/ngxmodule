af602fb677230508b59ab29b940239f275f00539
szqh97 <szqh97@163.com>
2017-03-11 22:44:42 +0800

    add tempfiles

---
+++ mytest/ngx_http_mytest_module.c
@@ -0,0 +1,10 @@
+#include "ngx_http_mytest_module.h"
+
+static char*
+ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+    ngx_http_core_loc_conf_t *clcf;
+    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+    clcf->handler = ngx_http_mytest_handler;
+    reutrn NGX_CONF_OK;
+}
